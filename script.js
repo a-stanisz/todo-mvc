333fba128df75ad8bdb0c4be36a5da282b12fb36
a-stanisz <hello@adamstaniszewski.com>
2020-10-01 13:49:58 +0200

    Add elements for the View, verbose way using DOM props and methods

--- script.js
+++ script.js
@@ -38,7 +38,43 @@ class Model {
 }
 
 class View {
-  constructor() {}
+  constructor() {
+    // The root element
+    this.app = this.getElement('#root');
+
+    // The App title
+    this.title = this.createElement('h1');
+    this.title.textContent = 'Todos';
+
+    // The form with an input and a submit button
+    this.form = this.createElement('form');
+    this.input = this.createElement('input');
+    this.input.type = 'text';
+    this.input.placeholder = 'Add TODO';
+    this.input.name = 'todo';
+    this.submitButton = this.createElement('button');
+    this.submitButton.textContent = 'Submit';
+
+    // The ul for visual representation of the TODO list
+    this.todoList = this.createElement('ul', 'todo-list');
+
+    // Append the input and the submit button to the form
+    this.form.append(this.input, this.submitButton);
+
+    // Append the title, form, and TODo list to the app
+    this.app.append(this.title, this.form, this.todoList);
+  }
+
+  createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (className) element.classList.add(className);
+    return element;
+  }
+
+  getElement(selector) {
+    const element = document.querySelector(selector);
+    return element;
+  }
 }
 
 class Controller {

4124e387df506f618fd27648399fc343ea4de660
a-stanisz <hello@adamstaniszewski.com>
2020-10-01 13:20:07 +0200

    Add methods to the Model class

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 class Model {
-  constructor() {}
+  constructor() {
+    this.todos = [
+      { id: 1, text: 'Fly to the Moon', complete: false },
+      { id: 2, text: 'Tide up the room', complete: false },
+    ];
+  }
+
+  addTodo(todoText) {
+    const todo = {
+      id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+      text: todoText,
+      complete: false,
+    };
+
+    this.todos.push(todo);
+  }
+
+  editTodo(id, updatedText) {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id
+        ? { id: todo.id, text: updatedText, complete: todo.complete }
+        : todo
+    );
+  }
+
+  deleteTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+
+  toggleTodo(id) {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id
+        ? { id: todo.id, text: todo.text, complete: !todo.complete }
+        : todo
+    );
+  }
 }
 
 class View {
